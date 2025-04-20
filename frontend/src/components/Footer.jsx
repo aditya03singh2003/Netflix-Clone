d09d09bd2c8456557ea8f75f87ee14ae268e1134
aditya03singh2003 <aditya.2201093cs@iiitbh.ac.in>
2025-04-21 01:14:12 +0530

    project completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,17 +3,17 @@ const Footer = () => {
 		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
-					Built by{" "}
+					Built by {""}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/aditya03singh2003'
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
-						you
+						  Aditya 
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/aditya03singh2003'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
